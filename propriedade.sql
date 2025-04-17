d79ab083c993795b5cfd7772a011e252f4d685f2
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2025-04-17 16:58:42 -0400

    Add files via upload
    + arquivo de importação que cria a tabela propriedade

---
+++ propriedade.sql
@@ -0,0 +1,12 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `propriedade` (
+  `NM_CHAVE` varchar(30) NOT NULL,
+  `NM_VALOR` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `propriedade`
+  ADD PRIMARY KEY (`NM_CHAVE`);
+COMMIT;
